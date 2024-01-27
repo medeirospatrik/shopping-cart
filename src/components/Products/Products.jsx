e9f480930b4f6491669072cd55b3f2bcb360008e
Patrik Medeiros <medeirospatri2@gmail.com>
2024-01-27 15:11:32 -0300

    tirando erro no cart

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -24,7 +24,7 @@ export default function Products() {
       console.log(response);
     });
 
-    setCartItems(getItemLocalStorage('cart'));
+    setCartItems(getItemLocalStorage('cart') || []);
   }, []);
 
   return (
